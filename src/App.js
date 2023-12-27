efbfd63fce1c4605e9b28990db1bcc47d47fa71b
Rahma Guedoiar <rahmaguedoiar@gmail.com>
2023-12-27 10:08:19 +0100

    chekpointredux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ListeTask from './ListeTask';
+import Addtask from './Addtask';
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Addtask></Addtask>
+      <ListeTask></ListeTask>
+      
+
     </div>
-  );
+  )
 }
 
 export default App;
